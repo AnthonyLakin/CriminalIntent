232cd94a0a3a97946e4986e08eb74388a98c50ae
anthonyPC <anthony.s.lakin@outlook.com>
2024-04-07 19:42:47 -0500

    Week 8 - 9

--- app/src/main/java/com/lakin/msu/criminalintent/CrimeDetailFragmentTest.kt
+++ app/src/main/java/com/lakin/msu/criminalintent/CrimeDetailFragmentTest.kt
@@ -4,9 +4,7 @@ import androidx.fragment.app.testing.FragmentScenario
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Before
-import org.junit.Test
 import org.junit.runner.RunWith
-import java.util.regex.Pattern.matches
 
 @RunWith(AndroidJUnit4::class)
 class CrimeDetailFragmentTest {
